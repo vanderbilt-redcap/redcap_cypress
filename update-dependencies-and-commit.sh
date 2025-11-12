4326fd1ea6238b60708b75266d5286d3a5bb56ce
Mark McEver <mark@fastmail.net>
2025-11-12 09:27:25 -0600

    Added a script to automate a common task I perform

---
+++ update-dependencies-and-commit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -n "$(git status --porcelain)" ]; then 
+  echo The redcap_cypress directory is not clean
+  exit
+fi
+
+rsvcBranch=`git -C redcap_rsvc rev-parse --abbrev-ref HEAD`
+if [ $rsvcBranch != 'marks-changes' ]; then
+    echo Unexpected branch for rsvc 
+    exit
+fi
+
+rctfPath='../../rctf'
+rctfBranch=`git -C $rctfPath rev-parse --abbrev-ref HEAD`
+if [ $rctfBranch != 'marks-changes' ]; then
+    echo Unexpected branch for rctf 
+    exit
+fi
+
+rsvcCommit=`git -C redcap_rsvc rev-parse HEAD`
+rctfCommit=`git -C $rctfPath rev-parse HEAD`
+
+npm install github:vanderbilt-redcap/redcap_rsvc#$rsvcCommit
+npm install github:vanderbilt-redcap/rctf#$rctfCommit
+npm link $rctfPath
+
+git add package.json package-lock.json
+git commit -m "rsvc to $rsvcCommit and rctf to $rctfCommit"
